5cdd9d0798eca36629a13b43c7b0e83f68fa6137
Davi Oliveira <davicabral94@gmail.com>
2020-10-28 20:15:18 -0300

    Initial Commit

---
+++ MovieTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieTestApp
+//
+//  Created by Davi Cabral de Oliveira on 28/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
